82e7c228f1d9526ffbbc15e68696f9586cb3f0e5
John Kidd <jwkidd3@Johns-Air-2.attlocal.net>
2021-11-05 14:47:27 -0500

    =new

---
+++ main.tf
@@ -0,0 +1,21 @@
+# main.tf
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+provider "aws" {
+  region = "us-east-1"
+}
+
+# declare a resource stanza so we can create something.
+resource "aws_s3_bucket_object" "user_student_alias_object" {
+  bucket  = "devint-${var.student_alias}"
+  key     = "student.alias"
+  content = "This bucket is reserved for ${var.student_alias}"
+}
+
---
+++ variables.tf
@@ -0,0 +1,8 @@
+# variables.tf
+
+# Declare a variable so we can use it.
+variable "student_alias" {
+  description = "Your student alias"
+  defaultt="user25"
+}
+
